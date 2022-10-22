abed896feea3f63b492d2375d57628728339166f
onurkopuz464 <onurkopuz464@gmail.com>
2022-10-22 15:03:02 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace c
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("sayi ; ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(sayi);
+            }
+            catch(Exception error)
+            {
+                 Console.WriteLine(error.Message.ToString());
+            }
+            finally
+            {
+                 Console.WriteLine("finally");
+            }
+
+            try
+            {
+                int a = int.Parse(null);
+                
+            }
+            catch (ArgumentException ex)
+            {
+                
+                 Console.WriteLine("Null");
+                  Console.WriteLine(ex);
+            }
+
+        }
+        
+      
+    }
+    
+}
